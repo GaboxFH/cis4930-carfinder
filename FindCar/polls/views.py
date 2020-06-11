c5ce826b7ed648aacc2ac52c22dbbd6b39ff57bf
kbradle <50191096+kbradle@users.noreply.github.com>
2020-06-10 22:48:57 -0400

    django setup

---
+++ FindCar/polls/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    """View function for home page of site."""
+   
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html')
+    
+# Create your views here.
+

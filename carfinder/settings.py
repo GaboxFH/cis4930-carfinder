dbfea922a12a3980ba44e962480ff172074aa313
Ziyad Morsi <zmorsi@ufl.edu>
2020-06-18 19:31:54 -0400

    fixing DIR

--- carfinder/settings.py
+++ carfinder/settings.py
@@ -126,5 +126,5 @@ STATIC_URL = '/static/'
 STATICFILES_STORAGE = "whitenoise.django.GzipManifestStaticFilesStorage"
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'finder/static/media')
 

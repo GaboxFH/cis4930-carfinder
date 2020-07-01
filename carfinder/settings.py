a2fa29fafa0b2a775fb30499f3285b97e18ef848
Ziyad Morsi <zmorsi@ufl.edu>
2020-06-30 20:51:49 -0400

    trurned debug to true

--- carfinder/settings.py
+++ carfinder/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'wqo*y0427o1pgag7vtlp3!)xtw2#!vmi56g!d47#h2&@dlt_tc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
